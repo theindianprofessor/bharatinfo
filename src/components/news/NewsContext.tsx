667a0f20c8adbd28a2e5d5361a78b95c9c3735fe
ThirstyBrain <shivam.dav1@gmail.com>
2025-03-04 19:52:50 +0530

    removed un used usestate

--- src/components/news/NewsContext.tsx
+++ src/components/news/NewsContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext,  ReactNode } from 'react';
 import useFetchNews from '../../hooks/news-service';
 
 interface NewsResponse {
